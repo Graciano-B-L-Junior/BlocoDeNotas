6626d30722af48e5dd1cf4741b39defb18a620a3
Graciano Junior <gracianobarbosalimajunior@gmail.com>
2020-04-22 17:38:01 -0300

    Add files via upload

--- bloco de notas/addNotas.js
+++ bloco de notas/addNotas.js
@@ -2,13 +2,23 @@ var btn = $(".add-icon");
 var display = $(".regiao");
 var notas =[]
 var id=1;
+var win = window.localStorage;
+var idSave=1;
+var qtd=1;
 
-btn.click(function(){
+//terminar o codigo que verificar a quantidade de ids quando o navegador fechar
+btn.click(function(){    
     var nota = $(`.Nota:first`).clone();
     notas[id] =nota;
     $(".regiao").append(notas[id])
     var trash= $(".trash").eq(id);
     $(trash).attr("myId",id)
+    var texto = $(".text").eq(id);
+    $(texto).change(function(){
+        var txt = $(texto).val();
+        win.setItem(id,txt)
+        console.log("boa noite "+txt)
+    })
     id++;
     $(trash).click(function(){
         ident = $(trash).attr("myId");
@@ -19,13 +29,43 @@ btn.click(function(){
             }
             
         }
-        console.log(ident+" ident");
-        console.log(id+" id")
     })
+    win.setItem(idSave,id);
 })
 
+window.onload = function(){
+    qtd = win.getItem(idSave);
+
+    for(i=1;i<qtd;i++){
+        
+        var nota = $(`.Nota:first`).clone();
+        notas[i] =nota;
+        $(".regiao").append(notas[i])
 
 
+        var trash= $(".trash").eq(i);
+        $(trash).attr("myId",i)
 
+        var texto = $(".text").eq(i);
+        var ide =i+1;
+        $(texto).text(win.getItem(ide))
 
+        $(texto).change(function(){
+            var txt = $(texto).val();
+            win.setItem(i,txt)
+        })
+        id = i;
+        $(trash).click(function(){
+            ident = $(trash).attr("myId");
+            console.log(ident)
+            for(i=0;i<=qtd;i++){
+                if(ident==i){                
+                    $(notas[ident]).remove();
+                    qtd--;
+                }
+                
+            }
+        })
+        }            
+}
 
